83197eef4c3bd19c111bd22fae7556a27921717d
Bluestar-34 <18981113554@163.com>
2025-05-04 12:15:37 +0800

    cebiankan'

---
+++ frontend/vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    port: 5173,
+    host: true,
+    allowedHosts: [
+      '789e-115-149-97-142.ngrok-free.app'
+    ]
+  }
+}) 
